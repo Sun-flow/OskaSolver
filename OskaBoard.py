d955a9b2e1f99210eebce9256d526cb85bcf1062
Jacie Mae <jcmoninghoff@gmail.com>
2020-05-08 16:40:22 -0700

    White Piece movement (no jumps)

--- OskaBoard.py
+++ OskaBoard.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 class Error(Exception):
     pass
 
@@ -16,12 +18,6 @@ class TooManyPiece(Error):
 
 class OskaBoard:
 
-    def class OskaPiece:
-    def __init__(self, char, row, col):
-        self.piece = char
-        self.row = row
-        self.col = col
-
     def __init__(self, inList, playerTurn):
    
         self.totalRows = len(inList)
@@ -39,27 +35,25 @@ class OskaBoard:
                         if len(inList[rowNum]) == self.maxRowLength - rowNum:
                             self.board += [row]
                         else:
-                            raise InvalidRowLength(1, rowNum, inList[rowNum])
+                            raise InvalidRowLength('First Half', rowNum, inList[rowNum])
                     else:
                         if len(inList[rowNum]) == 2 + pastCenter:
                             pastCenter += 1
                             self.board += [row]
                         else:
-                            raise InvalidRowLength(3, rowNum, inList[rowNum])
+                            raise InvalidRowLength('Second Half', rowNum, inList[rowNum])
 
                     rowNum += 1
 
             else:
                 raise InvalidInput(inList)
         
-            self.board.findPieces()
+            self.findPieces()
         
         except InvalidRowLength as inst:
             print('Invalid row length: ', inst.args)
         except InvalidInput as inst:
             print('Invalid input: ', inst.args)
-
-        
 
     
 
@@ -67,32 +61,121 @@ class OskaBoard:
         try:
             self.wPieces = []
             self.bPieces = []
+            wCount = 0
+            bCount = 0
             for row in range(len(self.board)):
-                for col in range(len(row)):
+                for col in range(len(self.board[row])):
                     char = self.board[row][col]
                     if char == 'W' or char == 'w':
-                        self.wPieces += OskaPiece(char, row, col)
+                        self.wPieces += [(row, col)]
+                        wCount += 1
                     elif char == 'B' or char == 'b':
-                        self.bPieces += OskaPiece(char, row, col)
+                        self.bPieces += [(row, col)]
+                        bCount += 1
                     elif char != '-':
-                        raise InvalidPiece(char, row, col)
+                        raise InvalidPiece(char, (row, col))
 
-            if len(self.wPieces) > self.maxRowLength:
+            if wCount > self.maxRowLength:
                 raise TooManyPiece('W', self.wPieces)
-            elif len(self.bPieces) > self.maxRowLength:
+            elif bCount > self.maxRowLength:
                 raise TooManyPiece('B', self.bPieces)
 
         except InvalidPiece as inst:
             print('Invalid Piece: ', inst.args)
         except TooManyPiece as inst:
-            print('Too many ', inst.args[0], 'pieces')
+            print('Too many', inst.args[0], 'pieces')
                 
-        
-
-
+    
 
     def printBoard(self):
         for i in range(len(self.board)):
             print('Row #', i, ': ', self.board[i])
 
         print('Player turn: ', self.playerTurn)
+
+
+    def replacechar(self, row, col, char):
+        newStr = ''
+
+        for i in range(len(self.board[row])):
+            if i == col:
+                newStr += char
+            else:
+                newStr += self.board[row][i]
+
+        self.board[row] = newStr
+
+
+    def movewhite(self):
+        newBoards = []
+        
+        for piece in self.wPieces:
+            index = self.wPieces.index(piece)
+            row = piece[0]
+            print(
+                'Piece:', piece,
+                '\nindex:', index
+            )
+            if row < (self.totalRows - 1) / 2:
+                newBoards += self.movedown(0, piece, index)
+            else:
+                newBoards += self.movedown(1, piece, index)
+        if newBoards != []:
+            return newBoards
+        else:
+            return None
+
+    def movedown(self, mode, piece, index):
+        newBoards = []
+        row = piece[0]
+        col = piece[1]
+        if mode == 0:
+            if col > 0 and row < (self.totalRows - 1) / 2 and self.board[row + 1][col - 1] == '-':
+
+                board = deepcopy(self)
+                
+                board.wPieces[index] = (row + 1, col - 1)
+                
+                board.replacechar(row, col, '-')
+                board.replacechar(row + 1, col - 1, 'w')
+                
+                newBoards += [board]
+
+            if col < len(self.board[row]) - 1 and row < self.totalRows and self.board[row + 1][col]:
+                board = deepcopy(self)
+
+                board.wPieces[index] = (row + 1, col)
+
+                board.replacechar(row, col, '-')
+                board.replacechar(row + 1, col, 'w')
+
+                newBoards += [board]
+        elif mode == 1:
+            if col >= 0 and row >= (self.totalRows - 1) / 2 and self.board[row + 1][col] == '-':
+
+                board = deepcopy(self)
+                
+                board.wPieces[index] = (row + 1, col)
+                
+                board.replacechar(row, col, '-')
+                board.replacechar(row + 1, col, 'w')
+                
+                newBoards += [board]
+
+            if col < len(self.board[row + 1]) and row >= (self.totalRows - 1) / 2 and self.board[row + 1][col + 1] == '-':
+                board = deepcopy(self)
+
+                board.wPieces[index] = (row + 1, col + 1)
+
+                board.replacechar(row, col, '-')
+                board.replacechar(row + 1, col + 1, 'w')
+
+                newBoards += [board]
+
+        return newBoards
+
+
+
+
+    def moveblack(inBoard):
+        newboards = []
--- oska.py
+++ oska.py
@@ -1,17 +1,47 @@
 import OskaBoard
-import OskaPiece
-        
+
+
+
+
+def generatechildren(inBoard, playerTurn):
+    newBoards = []
+    if playerTurn == 'w' or playerTurn == 'W':
+        newBoards = inBoard.movewhite()
+    elif playerTurn == 'b' or playerTurn == 'B':
+        newBoards = inBoard.moveblack()
+    else:
+        print('Invalid player: ', playerTurn)
+
+    if newBoards != None:
+        return newBoards
+    else:
+        return None
+
 
 
 def testinput():
 
     newBoard = OskaBoard.OskaBoard(['wwww','---','--','---','bbbb'], 'W')
-    newBoard.printBoard()
+    #newBoard.printBoard()
 
-    newBoard2 = OskaBoard.OskaBoard(['wwwww','----','---','--','---','----','bbbbb'], 'W')
-    newBoard2.printBoard()
+    newBoard2 = OskaBoard.OskaBoard(['ww-w-','----','---','-w','--w','----','bbbbb'], 'W')
+    #newBoard2.printBoard()
 
-    newBoard3 = OskaBoard.OskaBoard(['wwwww','----','---','---','----','bbbbb'], 'W')
+    #newBoard3 = OskaBoard.OskaBoard(['wwwww','----','---','---','----','bbbbb'], 'W')
 
-    newBoard4 = OskaBoard.OskaBoard(['wwww-w','---w-','----','---','--','---','-b--','-----','b-bbbb'], 'W')
-    newBoard4.printBoard()
+    #newBoard4 = OskaBoard.OskaBoard(['wwww-w','---w-','----','---','--','---','-b--','-----','b-bbbb'], 'W')
+    #newBoard4.printBoard()
+
+    #newBoard5 = OskaBoard.OskaBoard(['wwww','w--','--','---','bbbb'], 'W')
+    #newBoard5.printBoard()
+
+    #newBoard6 = OskaBoard.OskaBoard(['wwwq','---','--','---','bbbb'], 'W')
+    #newBoard6.printBoard()
+
+    newChildren = generatechildren(newBoard, 'w')
+    for child in newChildren:
+        child.printBoard()
+
+    newChildren2 = generatechildren(newBoard2, 'w')
+    for child in newChildren2:
+        child.printBoard()
